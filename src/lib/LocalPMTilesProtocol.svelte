f385ed64e1f2e9a881bb2ebf028f7032a2a0c3da
Lars Maxfield <maxfield.lars@gmail.com>
2025-02-13 11:01:11 +0100

    Minimal working local PMTilesProtocol

---
+++ src/lib/LocalPMTilesProtocol.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Protocol as MapLibreProtocol } from 'svelte-maplibre-gl';
+	import { PMTiles, Protocol } from 'pmtiles';
+
+  let {
+    scheme = 'pmtiles',
+    file
+  } = $props();
+	const protocol = new Protocol();
+  protocol.add(file);
+</script>
+
+<MapLibreProtocol {scheme} loadFn={protocol.tile} />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,40 @@
 <script lang="ts">
   import { MapLibre, RasterLayer, RasterTileSource } from 'svelte-maplibre-gl';
-  import { PMTilesProtocol } from 'svelte-maplibre-gl/pmtiles';
+  // import { PMTilesProtocol } from 'svelte-maplibre-gl/pmtiles';
   import { FileSource, PMTiles } from 'pmtiles';
+  import LocalPmTilesProtocol from '$lib/LocalPMTilesProtocol.svelte';
   import Select from '$lib/Select.svelte';
   let loaded = $state(false);
-  let files = $state();
+  let files = $state() as Array<File>;
+  let file = $derived.by( () => {
+    if (files) {
+      return new PMTiles( new FileSource(files[0]));
+    }
+    return undefined
+  }) as PMTiles;
+  let key = $derived(file ? file?.source?.getKey() : undefined)
+  // let key = $derived.by(() => {return file.source.getKey()})
+  // // "https://pmtiles.io/stamen_toner(raster)CC-BY+ODbL_z3.pmtiles"
+  let tiles = $derived([`pmtiles://${key}/{z}/{x}/{y}`])
+  // let tiles = $state() as Array<string>;
+  // [`pmtiles://${file.source.getKey()}/{z}/{x}/{y}`]
 
-  // $effect(() => (console.log(files)));
-  $effect(() => {
-    let file = new PMTiles( new FileSource(files[0]));
-    console.log(file);
-  });
+  $effect(() => (console.log(`house${files[0]}`)));
 </script>
 
-<PMTilesProtocol />
-
-<div class="main">  
+<div class="main">
   {#if loaded}
+  
+    <LocalPmTilesProtocol
+      file={file}
+    />
+
     <MapLibre
       inlineStyle="height: 100%; margin: 0px;"
       hash={true}
     >
       <RasterTileSource
-        url="pmtiles://https://pmtiles.io/stamen_toner(raster)CC-BY+ODbL_z3.pmtiles"
+        tiles={tiles}
       >
         <RasterLayer/>
       </RasterTileSource>
