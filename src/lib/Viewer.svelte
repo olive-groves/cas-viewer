e44de7ba92b4f0225cbdea506dffa171bc0b762f
Lars Maxfield <maxfield.lars@gmail.com>
2025-02-28 20:11:10 +0100

    Add sphere with direct control, too

--- src/lib/Viewer.svelte
+++ src/lib/Viewer.svelte
@@ -29,6 +29,7 @@
       contrast: 0.0
     },
     hillshade: {
+      angle: 315,
       exaggeration: 0.7,
       interval: 1,
       background: data?.raster.url ? 0 : 1,
@@ -39,23 +40,33 @@
     }
   })
 
-  $inspect(data?.raster.url).with(console.trace)
-
-
-  // Paint props with terrain on are not immediately reflected
+  // Certain paint props with terrain on are not immediately reflected
   // Reload here to do so
   $effect(() => {
     controls.rgb.brightness_max;
     controls.rgb.brightness_min;
     controls.rgb.contrast;
-    controls.hillshade.exaggeration;
     controls.hillshade.background;
     controls.hillshade.lightness;
     setTimeout(() => {
-        map?.terrain?.sourceCache.freeRtt();
-        map?.terrain?.sourceCache.sourceCache.reload();
+      map?.terrain?.sourceCache.freeRtt();
+      map?.terrain?.sourceCache.sourceCache.reload();
     }, 200);
   })
+
+  let timeout;  // Some paint props shouldn't be spammed, so we clear the timeout for those
+  $effect(() => {
+    controls.hillshade.exaggeration;
+    controls.hillshade.angle;
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+    timeout = setTimeout(() => {
+      map?.terrain?.sourceCache.freeRtt();
+      map?.terrain?.sourceCache.sourceCache.reload();
+    }, 200);
+  })
+
 
   $effect(() => {
     if (map) {
@@ -71,6 +82,54 @@
   })
 
   let overlay_visibilty = $derived(controls.overlay.visibility ? "visible" : "none")
+
+  let pointer = $state({
+    x: 0,
+    y: 0,
+    clientX: 0,
+    clientY: 0,
+    containerLeft: 0,
+    containerTop: 0,
+    containerWidth: 0,
+    containerHeight: 0,
+    roundedPercentX: 0,
+    roundedPercentY: 0,
+  })
+
+  $effect(() => {
+		pointer.x = pointer.clientX - pointer.containerLeft;
+		pointer.y = pointer.clientY - pointer.containerTop;
+	});
+
+  let radialX = $derived(pointer.x/pointer.containerWidth*100);
+  let radialY = $derived(pointer.y/pointer.containerHeight*100);
+
+  $effect(() => {
+    //
+    //   +x
+    //    ^     ·
+    //    |    /
+    //    | θ /
+    //    |  /
+    //    | /
+    //    |/
+    //————+———————————> +y
+    //    |
+    //
+    const mouseX = pointer.x / pointer.containerWidth;
+    const mouseY = pointer.y / pointer.containerHeight;
+    const mapCenterX = 0.5;
+    const mapCenterY = 0.5;
+    const deltaX = mouseX - mapCenterX;
+    const deltaY = mouseY - mapCenterY;
+    controls.hillshade.exaggeration = Math.min(Math.max(4 * Math.hypot(deltaX, deltaY), 0), 1)
+    const angle = Math.atan2(-deltaX, deltaY);  // Considered in a clockwise angle coordinate system where 0 degrees is pointing upwards
+    const angleDegrees = angle * (180.0 / Math.PI);
+    controls.hillshade.angle = Math.floor((angleDegrees + 180) % 360);  // Shift angle into range [0, 360) to match the input range for hillshade direction
+
+    pointer.roundedPercentX = Math.abs(deltaX) >= Math.abs(deltaY) ? 100*Math.round(mouseX) : 100*mouseX;
+    pointer.roundedPercentY = Math.abs(deltaY) >= Math.abs(deltaX) ? 100*Math.round(mouseY) : 100*mouseY;
+  })
 
   const getHeaderMetadata = async () => {
     const raster_header = await data?.raster.header 
@@ -108,7 +167,21 @@
       {/if}
     </tbody>
   </table> -->
-  <div class="container">
+
+  <div
+    class="container"
+    bind:clientWidth={pointer.containerWidth}
+    bind:clientHeight={pointer.containerHeight}
+    onpointermove={(e) => {
+      const rect = e.target?.getBoundingClientRect()
+      pointer.containerWidth = rect.width
+      pointer.containerHeight = rect.height
+      pointer.containerLeft = rect.left
+      pointer.containerTop = rect.top
+      pointer.clientX = e.clientX;
+      pointer.clientY = e.clientY;
+    }}
+  >
     <MapLibre
       bind:map={map}
       autoloadGlobalCss={false}
@@ -173,12 +246,12 @@
         />
         <HillshadeLayer
           paint={{
-            'hillshade-exaggeration': controls.hillshade.exaggeration,
-            'hillshade-shadow-color': "#000000",
-            'hillshade-accent-color': "#00000080",
-            'hillshade-highlight-color': "#ffffffff",
+            'hillshade-exaggeration': 1.0,
+            'hillshade-shadow-color': `rgba(0, 0, 0, ${Math.min(Math.max(2*controls.hillshade.exaggeration, 0), 1)})`,
+            'hillshade-accent-color': "rgba(0, 0, 0, 0)",
+            'hillshade-highlight-color': `rgba(255, 255, 255, ${Math.min(2*Math.max(controls.hillshade.exaggeration, 0), 1)})`,
             'hillshade-illumination-anchor': 'map',
-            'hillshade-illumination-direction': 0.0
+            'hillshade-illumination-direction': controls.hillshade.angle
           }}
         />
       </RasterDEMTileSource>
@@ -196,9 +269,9 @@
       </RasterTileSource>
     </MapLibre>
     <div
-      class="controls"
+      class="controls-container"
     >
-      <div class="container">
+      <div class="controls">
         <h3>NaN</h3>
         <label>
           <input type="checkbox" bind:checked={controls.overlay.visibility}>
@@ -234,30 +307,52 @@
           <input type="range" min=0 max=100 step=1 bind:value={controls.hillshade.lightness}>
           Background lightness ({controls.hillshade.lightness.toFixed(0)})
         </label>
+        <p>theta {controls.hillshade.angle}</p>
+        <figure
+          class="circle"
+          style={`background: radial-gradient(circle at ${radialX}px ${radialY}px, rgb(227, 227, 227) 0%, #000000 80%);`}
+          onpointermove={(e) => {
+            const rect = e.target?.getBoundingClientRect()
+            pointer.containerWidth = rect.width
+            pointer.containerHeight = rect.height
+            pointer.containerLeft = rect.left
+            pointer.containerTop = rect.top
+            pointer.clientX = e.clientX;
+            pointer.clientY = e.clientY;
+          }}
+        >
+        </figure>
       </div>
     </div>
   </div>
 {/await}
 
 <style>
+  .circle {
+    display: block;
+    background: black;
+    border-radius: 50%;
+    height: 100px;
+    width: 100px;
+    margin: 20px;
+  }
   .controls h3 {
     margin-bottom: 0;
     font-weight: normal;
   }
-  .controls .container {
+  .controls {
     display: flex;
     flex-direction: column;
     flex-wrap: nowrap;
     width: 400px;
   }
-  .controls .container * {
+  .controls * {
     pointer-events: auto;
   }
   .controls label, h3 {
-    white-space: nowrap;
     filter: drop-shadow(0px 0px 2px #000000);
   }
-  .controls {
+  .controls-container {
     filter: drop-shadow(0px 0px 2px #000000) drop-shadow(0px 0px 10px #000000) drop-shadow(0px 0px 100px #000000);
     pointer-events: none;
     z-index: calc(var(--z-index) + 1);
@@ -267,6 +362,7 @@
     flex-wrap: nowrap;
     grid-row-start: 1;
     grid-column-start: 1;
+    overflow: hidden;
   }
   .container {
     display: grid;
@@ -280,5 +376,6 @@
     padding: 0;
     position: var(--position, relative);
     clip-path: var(--clippath, none);
+    overflow: hidden;
   }
 </style>
