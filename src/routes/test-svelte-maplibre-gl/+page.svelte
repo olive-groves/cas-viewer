6a317c1528b0054d105ebb235e45bc37b99d1efb
Lars Maxfield <maxfield.lars@gmail.com>
2025-11-17 14:38:04 +0100

    Add test-svelte-maplibre-gl page

---
+++ src/routes/test-svelte-maplibre-gl/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import {
+    MapLibre,
+    RasterTileSource,
+    RasterDEMTileSource,
+    RasterLayer,
+    HillshadeLayer,
+    Terrain,
+    TerrainControl
+  } from 'svelte-maplibre-gl';
+</script>
+
+<MapLibre
+  inlineStyle="height: 100vh;"
+  zoom={12}
+  pitch={72}
+  maxPitch={72}
+  center={{ lng: 11.39085, lat: 47.3 }}
+>
+  <RasterDEMTileSource
+    id="terrain"
+    tiles={['https://demotiles.maplibre.org/terrain-tiles/{z}/{x}/{y}.png']}
+  >
+    <TerrainControl position="top-right" />
+    <Terrain />
+  </RasterDEMTileSource>
+  <RasterTileSource
+    id="terrain1"
+    tiles={['https://demotiles.maplibre.org/terrain-tiles/{z}/{x}/{y}.png']}
+  >
+    <RasterLayer />
+  </RasterTileSource>
+  <RasterDEMTileSource
+    id="terrain2"
+    tiles={['https://demotiles.maplibre.org/terrain-tiles/{z}/{x}/{y}.png']}
+  >
+    <HillshadeLayer />
+  </RasterDEMTileSource>
+</MapLibre>
