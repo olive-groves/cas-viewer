5c33b88623e6fbfb5b5c1a5e5a0faa6a12f4ad4a
Lars Maxfield <maxfield.lars@gmail.com>
2026-03-02 15:57:02 +0100

    Add colorbar div testbed

--- src/routes/lib/colorbar/+page.svelte
+++ src/routes/lib/colorbar/+page.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
-    import { getScientific, getSmallestGreaterThanOrEqualTo, range } from "$lib/Mathematics.svelte";
+    import { getScientific, getSmallestGreaterThanOrEqualTo, range, type NumberScientific } from "$lib/Mathematics.svelte";
 
-  let min = $state(0);
-  let max = $state(5);
-  let n_major_max = $state(12);
+  let colorbar_length_pixels = $state(400)
 
-  let units_major_unrounded = $derived((max - min)/(n_major_max + 1))
-  let units_major = $derived.by(() => {
+  let min = $state(-1.1);
+  let max = $state(0.387);
+  let n_max = $state(15);
+
+  let units_unrounded = $derived((max - min)/(n_max + 1))
+  let units_scientific: NumberScientific = $derived.by(() => {
     let significand: number;
     let exponent: number;
-    const unrounded_scientific = getScientific(units_major_unrounded)
+    const unrounded_scientific = getScientific(units_unrounded)
     if (unrounded_scientific.significand === 0) {
         significand = 0;
         exponent = 0;
@@ -21,31 +23,111 @@
           exponent += 1;
       }
     }
-    return significand * 10 ** exponent
+    return {significand, exponent}
   })
-  let n_major = $derived(units_major === 0 ? 0 : Math.floor(1 + (max/units_major) - Math.ceil(min/units_major)))
+  let units = $derived(units_scientific.significand * 10 ** units_scientific.exponent)
+  let n = $derived(units === 0 ? 0 : Math.floor(1 + (max/units) - Math.ceil(min/units)))
 
-  let major_ticks_start = $derived(Math.ceil(min/units_major) * units_major)
-  let major_ticks = $derived(range(n_major).map((i) => major_ticks_start + i * units_major))
+  let ticks_start = $derived(Math.ceil(min/units) * units)
+  let ticks = $derived(range(n).map((i) => ticks_start + i * units))
 </script>
 
 <label>
-  <input type=range bind:value={min} min={0} max={max} step={0.01}>
+  <input type=range bind:value={min} min={-2} max={max} step={0.001}>
   {min}
 </label>
 <label>
-  <input type=range bind:value={max} min={min} max={10} step={0.01}>
+  <input type=range bind:value={max} min={min} max={2} step={0.001}>
   {max}
 </label>
 
-<p>{units_major_unrounded} major units unrounded</p>
-<p>{units_major} major units</p>
-<p>{n_major} number of major ticks</p>
+<p>{units_unrounded} units unrounded</p>
+<p>{units} units</p>
+<p>{n} number of ticks</p>
 
-<ul>
-  {#each major_ticks as major_tick}
+<label>
+  <input type=range bind:value={colorbar_length_pixels} min={0} max={1000} step={10}>
+  {colorbar_length_pixels}px
+</label>
+<!--
+  Tick position as a percentage of the length of a div?
+  [0.271] [0.3, 0.4, 0.5] [0.5]
+  0.3 -> 12.6%
+  0.4 -> 56.3%
+  0.5 -> 100.0%
+  The idea is to define a div (a colorbar) where the start is the min, the end is max,
+  and the ticks are placed relative to the start and end.
+-->
+
+<div class=figure>
+  <div class=plot>
+    <!-- <div class=axes>
+    </div> -->
+  </div>
+  <div class=colorbar style:height={`${colorbar_length_pixels}px`}>
+    <div class=scale>
+    </div>
+    <div class=ticks>
+      {#each ticks as tick}
+        <div class=tick>
+          {tick.toFixed(units_scientific.exponent >= 0 ? 0 : Math.abs(units_scientific.exponent))}
+            <!-- {tick} -->
+        </div>
+      {/each}
+    </div>
+  </div>
+</div>
+
+<!-- <ul class=ticks>
+  {#each ticks.toReversed() as tick}
   <li>
-    {major_tick}
+    {tick.toFixed(units_scientific.exponent >= 0 ? 0 : Math.abs(units_scientific.exponent))}
   </li>
   {/each}
-</ul>
+</ul> -->
+
+<style>
+  .figure {
+    box-sizing: border-box;
+    display: flex;
+    padding: 10px;
+    width: 100%;
+    flex-wrap: nowrap;
+    border: 1px solid gray;
+    .plot {
+      flex-grow: 1;
+      border: 1px solid gray;
+      padding: 10px;
+      background-clip: content-box;
+      background: radial-gradient(
+          #fde725,
+          #5ec962,
+          #21918c,
+          #3b528b,
+          #440154
+      )
+    }
+    .colorbar {
+      display: flex;
+      border: 1px solid gray;
+      padding: 10px;
+      .ticks {
+        display: flex;
+        flex-wrap: nowrap;
+        flex-direction: column-reverse;
+        background-color: gray;
+      }
+      .scale {
+        border: 1px solid white;
+        width: 10px;
+        background: linear-gradient(
+          #fde725,
+          #5ec962,
+          #21918c,
+          #3b528b,
+          #440154
+        );
+      }
+    }
+  }
+</style>
