d664b6a3e816f60bc854152604e8d773ad64324f
Lars Maxfield <maxfield.lars@gmail.com>
2026-05-11 13:10:42 +0200

    Terradraw: Fix auto-edit selected mode bug

--- src/routes/test/terradraw/+page.svelte
+++ src/routes/test/terradraw/+page.svelte
@@ -8,15 +8,13 @@
     TerraDrawLineStringMode,
     TerraDrawPolygonMode,
     TerraDrawPointMode,
-    // TerraDrawCircleMode,
-    // TerraDrawRectangleMode,
     TerraDrawSessionUndoRedo,
     TerraDrawModeUndoRedo,
     TerraDrawUndoRedoKeyboardShortcuts,
   } from 'terra-draw';
-  import { untrack } from 'svelte';
+    import { untrack } from 'svelte';
 
-  // TODO: Make into enhanced terradraw componenent
+  // TODO: Make into enhanced, predefaulted terradraw Svelte component
   // Props:
   //  autoEdit
   // Passing modename with 'auto-edit' flag means it will always autoedit
@@ -30,6 +28,11 @@
   let _lastDrawId: FeatureId | undefined = $state(undefined);
   let _lastDrawMode: string | undefined = $state();
 
+  const undoRedo = {
+    modeLevel: new TerraDrawModeUndoRedo({ maxStackSize: 100 }),
+    sessionLevel: new TerraDrawSessionUndoRedo({ maxStackSize: 100 }),
+    keyboardShortcuts: new TerraDrawUndoRedoKeyboardShortcuts(),
+  };
   const defaultSelectFlags = {
     feature: {
       draggable: true,
@@ -44,7 +47,7 @@
     point: defaultSelectFlags,
     linestring: defaultSelectFlags,
     polygon: defaultSelectFlags,
-    'linestring-auto-edit': defaultSelectFlags,
+    'linestring--auto-edit--profile': defaultSelectFlags,
   }
   // Select mode for the last drawn item, which is used to auto-select it, but not allow other selections
   const lastDrawSelectMode = new TerraDrawSelectMode({
@@ -61,31 +64,41 @@
     new TerraDrawPolygonMode(),
     new TerraDrawLineStringMode({
       finishOnNthCoordinate: 2,
-      modeName: 'linestring-auto-edit',
+      modeName: 'linestring--auto-edit--profile',
     }),
-    // new TerraDrawCircleMode(),
-    // new TerraDrawRectangleMode(),
   ];
   const modeNames = modes.map((mode) => mode.mode);
-  let mode = $state('linestring-auto-edit')
-  const undoRedo = {
-    modeLevel: new TerraDrawModeUndoRedo({ maxStackSize: 100 }),
-    sessionLevel: new TerraDrawSessionUndoRedo({ maxStackSize: 100 }),
-    // keyboardShortcuts: new TerraDrawUndoRedoKeyboardShortcuts(),
-  };
+  let mode = $state('linestring--auto-edit--profile');  // Public prop
   let selected: string | number | null = $state(null);
+  // KEEP:
+  // An error occurs if switching modes when a feature is selected during auto-edit.
+  // For now the fix is to explicitly deselect the feature when we change modes.
+  // svelte-ignore state_referenced_locally
+  let _mode = $state(mode);
+  $effect(() => {
+    const _selected = untrack(() => selected);
+    if (_selected) draw?.deselectFeature(_selected);
+    _mode = mode;
+  })
 </script>
 
 <MapLibre
   inlineStyle="height: 100%;"
-  style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
+  // style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
   zoom={2}
   center={{ lng: 60, lat: 20 }}
   renderWorldCopies={false}
+  onload={() => {
+    // Auto-save
+    // const retrievedFeatures = localStorage.getItem('terra-draw-data');
+    // if (retrievedFeatures) {
+    //   draw?.addFeatures(JSON.parse(retrievedFeatures))
+    // }
+  }}
 >
   <!-- Terra Draw -->
   <TerraDraw
-    {mode}
+    mode={_mode}
     modes={[...modes, lastDrawSelectMode]}
     {undoRedo}
     bind:draw
@@ -94,17 +107,39 @@
     }}
     ondeselect={(id: FeatureId) => {
       selected = null;
+      // Auto-edit
       if (id === _lastDrawId && _lastDrawMode) {
         _lastDrawId = undefined;
         draw?.setMode(_lastDrawMode);
       }
     }}
     onfinish={(id: FeatureId, context?) => {
-      if (context?.action === 'draw' && (draw?.getMode().includes('auto-edit') || autoEdit)) {
+      // Auto-save
+      // const features = draw?.getSnapshot()
+      // if (features) {
+      //   // We don't want any mid points or selection points so we filter them out
+      //   const filteredFeatures = features.filter((f) => !f.properties.midPoint && !f.properties.selectionPoint)
+      //   // localStorage can only store strings, so we stringify the features first
+      //   localStorage.setItem('terra-draw-data', JSON.stringify(filteredFeatures));
+      // }
+  
+      // Auto-edit
+      if (
+        context?.action === 'draw' &&
+        (
+          context?.mode.includes('--auto-edit') ||
+          autoEdit
+        ) 
+      ) {
         _lastDrawId = id;
         _lastDrawMode = mode;
         draw?.selectFeature(id, lastDrawSelectMode.mode);
       }
+      
+      // Profile tool
+      // If linestring-auto-edit finishes, register profile and plot?
+      // if (context?.action === "draw" && draw?.getSnapshotFeature(id[0]))
+      // console.log(context)
     }}
     onchange={(ids: FeatureId[], type: string, context?) => {
     }}
