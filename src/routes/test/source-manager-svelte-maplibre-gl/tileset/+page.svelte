c50780aec7fa003a19b6a6ede9e85742d177acae
Lars Maxfield <maxfield.lars@gmail.com>
2026-06-12 22:47:35 +0200

    Add source->mapLibreSource tileset proof
    Might be an XY problem; rename original tileset proof

--- src/routes/test/source-manager-svelte-maplibre-gl/tileset/+page.svelte
+++ src/routes/test/source-manager-svelte-maplibre-gl/tileset/+page.svelte
@@ -2,51 +2,86 @@
   import { SvelteMap } from 'svelte/reactivity';
   import { PMTilesTileset, MapLibreSourceSpecAdapter, RemotePMTilesTileset } from '$lib/tileset';
   import { PMTilesProtocol } from '@svelte-maplibre-gl/pmtiles';
+  import { untrack } from 'svelte';
 
-  const initial_paths = [
-    '/local/bagunca-2025-10-21T1629/rgb.pmtiles',
-    // '/local/bagunca-2025-10-21T1629/height.pmtiles',
-    // '/local/bagunca-2025-10-21T1629/nan.pmtiles',
-    // 'https://tiles.larsmaxfield.com/paintings/almond-blossom/20250107-1604/20250520_153658/rgb.pmtiles',
-  ];
+  // '/local/bagunca-2025-10-21T1629/height.pmtiles',
+  // '/local/bagunca-2025-10-21T1629/nan.pmtiles',
+  const localUrl = new URL('/local/bagunca-2025-10-21T1629/rgb.pmtiles', import.meta.url);
+  const remoteUrl = new URL('https://tiles.larsmaxfield.com/paintings/almond-blossom/20250107-1604/20250520_153658/rgb.pmtiles');
+  const initialUrls = [localUrl, remoteUrl]
 
-  // This:
-  // const sources = new SvelteMap([
-  //   [crypto.randomUUID(), new MapLibreSourceSpecAdapter(new RemotePMTilesTileset(initial_paths[0]))],
-  // ])
-  // ...but dynamically:
-  const sources = new SvelteMap(
-    initial_paths.map((path) => [
-      crypto.randomUUID(),
-      new MapLibreSourceSpecAdapter(new RemotePMTilesTileset(initial_paths[0]))
-    ])
+  const sources: SvelteMap<string, PMTilesTileset> = new SvelteMap(
+    initialUrls.map((url) => [crypto.randomUUID(), sourceFromUrl(url)])
   )
 
-  const addSource = () => sources.set(crypto.randomUUID(), new MapLibreSourceSpecAdapter(new RemotePMTilesTileset(initial_paths[Math.floor(Math.random()*initial_paths.length)])));
+  // const mapLibreSources: [string, MapLibreSourceSpecAdapter][] = $derived(
+  //   sources.entries().map(([key, source]) => [key, new MapLibreSourceSpecAdapter(source)])
+  // );
+
+  const mapLibreSources: SvelteMap<string, MapLibreSourceSpecAdapter> = new SvelteMap();
+
+  $effect(() => {
+    const m = untrack(() => mapLibreSources)
+
+    // Which keys in sources are missing in mapLibreSources? Which are now gone?
+    const sourcesKeys = new Set(sources.keys());
+    const mapLibreSourcesKeys = new Set(m.keys());
+    const missingMapLibreSources = sourcesKeys.difference(mapLibreSourcesKeys);
+    const removedSources = mapLibreSourcesKeys.difference(sourcesKeys);
+
+    missingMapLibreSources.forEach((sourceKey) => mapLibreSources.set(sourceKey, new MapLibreSourceSpecAdapter(sources.get(sourceKey))));
+    removedSources.forEach((sourceKey) => mapLibreSources.delete(sourceKey));
+  })
+
+
+  function sourceFromUrl(url: URL): PMTilesTileset {
+    const pathname = url.pathname.toLowerCase();
+
+    if (pathname.endsWith(".json")) {
+      // return new TileJSONTileset(url);
+      throw Error("JSON not yet supported")
+    } else if (pathname.endsWith(".pmtiles")) {
+      return new RemotePMTilesTileset(url.toString());
+    } else {
+      throw Error("Unable to parse source from url")
+    }
+  };
+
+  function addSource(source) {sources.set(crypto.randomUUID(), source)};
+
 </script>
 
 <!-- <PMTilesProtocol /> -->
 
-<button onclick={addSource}>Another One</button>
-{#each sources.entries() as [key, source] (key)}
-  <div>
-    {key}
-    {#if source.source.format === "pmtiles"}
-      {#await source.source.header then header}
-        {#each Object.entries(header) as [key, value]}
-          <div>{key}: {value}</div>
-        {/each}
-      {/await}
-      {#await source.source.metadata then metadata}
-        {#each Object.entries(metadata) as [key, value]}
-          <div>{key}: {value}</div>
-        {/each}
-      {/await}
-    {/if}
-    {#await source.spec then spec}
-      {#each Object.entries(spec) as [key, value]}
-        <div>{key}: {value}</div>
-      {/each}
-    {/await}
+<div style={'height: 100%; overflow: hidden; display: flex; flex-direction: column;'}>
+  <div style={'display: flex;'}>
+    <button onclick={() => addSource(sourceFromUrl(localUrl))}>Add local</button>
+    <button onclick={() => addSource(sourceFromUrl(remoteUrl))}>Add remote</button>
+    <button onclick={() => sources.delete([...sources.keys()][0])}>Delete</button>
+    <!-- <button onclick={() => addSource(sourceFromUrl(remoteUrl))}>Add remote</button> -->
   </div>
-{/each}
+  <div style={'overflow-y: scroll; flex: 1;'}>
+  {#each mapLibreSources as [key, mapLibreSource] (key)}
+    <div>
+      {key}
+      {#if mapLibreSource.source.format === "pmtiles"}
+        {#await mapLibreSource.source.header then header}
+          {#each Object.entries(header) as [key, value]}
+            <div>{key}: {value}</div>
+          {/each}
+        {/await}
+        {#await mapLibreSource.source.metadata then metadata}
+          {#each Object.entries(metadata) as [key, value]}
+            <div>{key}: {value}</div>
+          {/each}
+        {/await}
+      {/if}
+      {#await mapLibreSource.spec then spec}
+        {#each Object.entries(spec) as [key, value]}
+          <div>{key}: {value}</div>
+        {/each}
+      {/await}
+    </div>
+  {/each}
+  </div>
+</div>
---
+++ src/routes/test/source-manager-svelte-maplibre-gl/tileset/proof+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import { SvelteMap } from 'svelte/reactivity';
+  import { PMTilesTileset, MapLibreSourceSpecAdapter, RemotePMTilesTileset } from '$lib/tileset';
+  import { PMTilesProtocol } from '@svelte-maplibre-gl/pmtiles';
+
+  const initial_paths = [
+    '/local/bagunca-2025-10-21T1629/rgb.pmtiles',
+    // '/local/bagunca-2025-10-21T1629/height.pmtiles',
+    // '/local/bagunca-2025-10-21T1629/nan.pmtiles',
+    // 'https://tiles.larsmaxfield.com/paintings/almond-blossom/20250107-1604/20250520_153658/rgb.pmtiles',
+  ];
+
+  // This:
+  // const sources = new SvelteMap([
+  //   [crypto.randomUUID(), new MapLibreSourceSpecAdapter(new RemotePMTilesTileset(initial_paths[0]))],
+  // ])
+  // ...but dynamically:
+  const sources = new SvelteMap(
+    initial_paths.map((path) => [
+      crypto.randomUUID(),
+      new MapLibreSourceSpecAdapter(new RemotePMTilesTileset(initial_paths[0]))
+    ])
+  )
+
+  const addSource = () => sources.set(crypto.randomUUID(), new MapLibreSourceSpecAdapter(new RemotePMTilesTileset(initial_paths[Math.floor(Math.random()*initial_paths.length)])));
+</script>
+
+<!-- <PMTilesProtocol /> -->
+
+<button onclick={addSource}>Another One</button>
+{#each sources.entries() as [key, source] (key)}
+  <div>
+    {key}
+    {#if source.source.format === "pmtiles"}
+      {#await source.source.header then header}
+        {#each Object.entries(header) as [key, value]}
+          <div>{key}: {value}</div>
+        {/each}
+      {/await}
+      {#await source.source.metadata then metadata}
+        {#each Object.entries(metadata) as [key, value]}
+          <div>{key}: {value}</div>
+        {/each}
+      {/await}
+    {/if}
+    {#await source.spec then spec}
+      {#each Object.entries(spec) as [key, value]}
+        <div>{key}: {value}</div>
+      {/each}
+    {/await}
+  </div>
+{/each}
