e8fb5d987ca08dd4e5b35fa2f008fd3be02b783f
Lars Maxfield <maxfield.lars@gmail.com>
2026-06-19 15:32:57 +0200

    OrderedSvelteMap: Move to utils.svelte.ts, lib
    Remove defunct layer-group proofs

---
+++ src/lib/utils.svelte.ts
@@ -0,0 +1,60 @@
+import { SvelteMap } from "svelte/reactivity";
+
+export class OrderedSvelteMap<V> {
+  map: SvelteMap<string, V> = new SvelteMap();
+  order: string[] = $state([]);
+  // Whether to reorder the key in place upon add and move, allowing tab focus to be retained* in #each blocks.
+  // *This requires you to track the previously focused element, for which there is an example in /routes/lib.
+  private reorderItemInPlace: boolean;
+
+  constructor(initialValues?: V[], options = {reorderItemInPlace: false}) {
+    this.reorderItemInPlace = options.reorderItemInPlace;
+    if (typeof initialValues !== 'undefined') {
+      initialValues.forEach((value) => this.add(value));
+    }
+  }
+
+  generateKey(): string {
+    return crypto.randomUUID()
+  }
+
+  add(value: V, options?: {index?: number}) {
+    const key = this.generateKey();
+    this.map.set(key, value)
+
+    const to = options?.index ?? this.order.length;
+    const toRemainder = to % (this.order.length + 1);
+    if (this.reorderItemInPlace) {
+      this.order = this.order.toSpliced(toRemainder < 0 ? this.order.length + toRemainder : toRemainder, 0, key);
+    } else {
+      this.order.splice(toRemainder < 0 ? this.order.length + toRemainder : toRemainder, 0, key)
+    }
+
+    return key
+  }
+
+  delete(keyOrIndex: string | number): boolean {
+    let key, index;
+    if (typeof keyOrIndex === "number") {
+      index = keyOrIndex;
+      key = this.order.at(index);
+    } else {
+      key = keyOrIndex;
+      index = this.order.indexOf(key);
+    }
+    if (index < 0) return false
+    this.order.splice(index, 1);
+    return key ? this.map.delete(key) : false;
+  }
+
+  move(from: number, to: number) {
+    const toRemainder = to % this.order.length;
+    if (toRemainder === from) return;
+    if (this.reorderItemInPlace) {  // Slower (copies), but retains focus
+      this.order = this.order.toSpliced(from, 1).toSpliced(toRemainder < 0 ? this.order.length + toRemainder : toRemainder, 0, this.order[from]);
+    } else {  // Faster (mutates), but does not retain focus
+      this.order.splice(toRemainder < 0 ? this.order.length + toRemainder : toRemainder, 0, this.order.splice(from, 1)[0])
+    }
+  }
+
+}
---
+++ src/routes/lib/ordered-svelte-map/+page.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import { untrack } from "svelte";
+  import { flip } from "svelte/animate";
+  import { slide, fly } from "svelte/transition";
+
+  import { OrderedSvelteMap } from "$lib/utils.svelte";
+
+  class Value {
+    id = crypto.randomUUID();
+  }
+  const initial_n = 5;
+  const initial_values = Array.from(Array(initial_n), (_) => new Value());
+  const valuesMap = new OrderedSvelteMap(initial_values, {reorderItemInPlace: true});
+
+  // Keep tab focus on move
+  let previousActiveElement: HTMLElement | HTMLLIElement | null = $state(null);
+	$effect(() => {
+		void valuesMap.order;  // $state array(s) whose order will be rearranged
+    let gaveFocusToPrevious = false;
+		untrack(() => {
+      if ((document?.activeElement === document?.body) && previousActiveElement) {  // Reorder moves focus to body, so we set to previous
+        previousActiveElement.focus();
+        gaveFocusToPrevious = true;
+      }
+		});
+    if (gaveFocusToPrevious) previousActiveElement = null;
+	});
+</script>
+
+<div style:display=flex style:flex-direction=column>
+  <button onclick={() => valuesMap.move(0, -1)}>Move top to bottom</button>
+  <button onclick={() => valuesMap.add(new Value(), {index: 0})}>Add to start</button>
+  <button onclick={() => valuesMap.add(new Value())}>Add to end</button>
+  {#each valuesMap.order as key, index (key)}
+    {@const value = valuesMap.map.get(key)}
+    <div style:display=flex animate:flip in:fly out:slide>
+      <div>
+        <button onclick={() => valuesMap.move(index, index - 1)} onfocusout={(e) => previousActiveElement = e.target as HTMLButtonElement}>
+          ▲
+        </button>
+      </div>
+      <div>
+        <button onclick={() => {valuesMap.move(index, index + 1)}} onfocusout={(e) => previousActiveElement = e.target as HTMLButtonElement}>
+          ▼
+        </button>
+      </div>
+      <div>
+        <button onclick={() => {valuesMap.delete(index)}}>
+          ×
+        </button>
+      </div>
+      <div style:margin="0px 8px">{index}</div>
+      <div style:flex=1>{value.id}</div>
+    </div>
+  {/each}
+</div>
+
