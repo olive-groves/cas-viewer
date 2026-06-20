62fa6e64727942407cd545bd1d4185012f7be784
Lars Maxfield <maxfield.lars@gmail.com>
2026-06-20 09:24:44 +0200

    Confirm refetch issue with RasterDEMSource in each
    But it still properly shares source with layers. We'll live with that.

--- src/routes/test/layer-group/debug/+page.svelte
+++ src/routes/test/layer-group/debug/+page.svelte
@@ -8,7 +8,7 @@
 
 
   import { PMTilesProtocol } from '@svelte-maplibre-gl/pmtiles';
-  import { ColorReliefLayer, HillshadeLayer, MapLibre, RasterDEMTileSource, RasterLayer, RasterTileSource } from 'svelte-maplibre-gl';
+  import { ColorReliefLayer, HillshadeLayer, MapLibre, RasterDEMTileSource, RasterLayer, RasterTileSource, NavigationControl } from 'svelte-maplibre-gl';
 
   // A layer group defines a map of layers with a particular order.
   // You start with an empty group.
@@ -67,8 +67,6 @@
 
   const localUrl = new URL('/local/bagunca-2025-10-21T1629/rgb.pmtiles', import.meta.url);
   const localUrlDem = new URL('/local/bagunca-2025-10-21T1629/height.pmtiles', import.meta.url);
-  const remoteUrl = new URL('https://tiles.larsmaxfield.com/paintings/almond-blossom/20250107-1604/20250520_153658/rgb.pmtiles');
-  const remoteUrlDem = new URL('https://tiles.larsmaxfield.com/paintings/almond-blossom/20250107-1604/20250520_153658/height.pmtiles');
   const initialUrls = [localUrl, localUrlDem]
 
   const sourceManager = new SourceManager();
@@ -91,27 +89,69 @@
     }
   };
 
-  function addSource(source) {sourceManager.add(source, {mapLibre: {override: {type: "raster-dem"}}})};
+  function addSource(source) {sourceManager.add(source)};
+  function addLayer(i) {mapLibreLayerGroup.add({source_key: [...sourceManager.mapLibreSources.keys()][i]})}
+
+   // Depends on the map, not the order
+  const mapLibreLayerGroup = new OrderedSvelteMap();
+  const uniqueSourceKeys = $derived(
+    [...new Set(Array.from(mapLibreLayerGroup.map.values(), (value) => value.source_key))]
+  )
+
+  let countLayers = $state([
+    {i: 0, message: "nice"}
+  ])
 
 </script>
 
-<PMTilesProtocol pmtiles={[...sourceManager.sources.values()].map((value) => value.archive)} />
+<PMTilesProtocol />
 
 <div style={'height: 100%; overflow: hidden; display: flex; flex-direction: column;'}>
+  <div>
+    Count
+    <button onclick={() => countLayers.push({i: countLayers.length, message: "nice"})}>Add</button>
+  </div>
   <div style={'display: flex;'}>
-    Add
+    Add source
     <button onclick={() => addSource(sourceFromUrl(localUrl))}>Local</button>
     <button onclick={() => addSource(sourceFromUrl(localUrlDem))}>Local DEM</button>
   </div>
-  <div style={'display: flex; height: 200px;'}>
+  <div style={'display: flex;'}>
+    Add layer
+    <button onclick={() => addLayer(0)}>Local</button>
+    <button onclick={() => addLayer(1)}>Local DEM</button>
+  </div>
+  <div style={'display: flex; height: 100%;'}>
     <MapLibre
       inlineStyle="height: 100%; width: 100%;"
       renderWorldCopies={false}
       aroundCenter={false}
       transformConstrain={(lngLat, zoom) => ({center: lngLat, zoom})}
-      zoom={-2}
+      // zoom={-2}
     >
-      {@const sourceKey = [...sourceManager.mapLibreSources.keys()][0]}
+      <NavigationControl />
+      {#each sourceManager.mapLibreSources as [key, mapLibreSource] (key)}
+        {#await mapLibreSource.source.spec then spec}
+          {#if spec.type === "raster-dem"}
+            <RasterDEMTileSource
+              {...{...spec, id: key}}
+            >
+              {#each countLayers as {i, message} (i)}
+              <HillshadeLayer source={key} />
+              {/each}
+            </RasterDEMTileSource>
+          {:else if spec.type === "raster"}
+            <RasterTileSource
+              {...{...spec, id: key}}
+            >
+              {#each countLayers as {i, message} (i)}
+              <RasterLayer source={key} paint={{"raster-opacity": 0.1}} />
+              {/each}
+            </RasterTileSource>
+          {/if}
+        {/await}
+      {/each}
+      <!-- {@const sourceKey = [...sourceManager.mapLibreSources.keys()][0]}
       {@const override = sourceManager.mapLibreSources.get(sourceKey)?.override}
       {#await [...sourceManager.mapLibreSources.values()].at(0)?.source.spec then spec}
         <RasterDEMTileSource
@@ -119,7 +159,7 @@
         >
           <HillshadeLayer/>
         </RasterDEMTileSource>
-      {/await}
+      {/await} -->
     </MapLibre>
   </div>
 </div>
