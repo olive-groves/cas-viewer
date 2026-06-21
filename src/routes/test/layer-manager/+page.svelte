5aedf4ea27e5085c46a92d7a30b2cc7ccbfaaa33
Lars Maxfield <maxfield.lars@gmail.com>
2026-06-21 17:40:19 +0200

    Improve typing on spec, overrides

--- src/routes/test/layer-manager/+page.svelte
+++ src/routes/test/layer-manager/+page.svelte
@@ -24,7 +24,7 @@
   // id={`slot-{i}`}
   // Each Viewer has a LayerGroup / LayerManager?
 
-  // Apparently we need DeepPartial on nested objects?
+  // For nested objects
   type DeepPartial<T> = T extends any[] ? T : { [P in keyof T]?: DeepPartial<T[P]> }
 
   // Generic Background for layers, not as a MapLibre spec, though it is meant for adding backgrounds to slots.
@@ -36,42 +36,36 @@
 
   type LayerType = maplibregl.LayerSpecification["type"];
 
-
-  type DistributiveOmit<T, K extends PropertyKey> = T extends
-    unknown
-    ? Omit<T, Extract<keyof T, K>>
-    : never;
-
-  type DistributiveMakeOptional<T, K extends PropertyKey> = T extends
+  type MakeOptional<T, K extends PropertyKey> = T extends
     unknown
     ? Omit<T, Extract<keyof T, K>> & Partial<Pick<T, Extract<keyof T, K>>>
     : never;
 
-  type LayerSpecByType<TType extends LayerType> = DistributiveMakeOptional<
+  type LayerSpecByType<TType extends LayerType> = MakeOptional<
     Extract<maplibregl.LayerSpecification, { type: TType }>,
     "id"
   >;
 
-  type LayerOverride<TType extends LayerType> = {
-    // spec: DeepPartial<DistributiveOmit<LayerSpecByType<TType>, "type">>;  // Don't omit type for now...
-    spec: DeepPartial<LayerSpecByType<TType>>;
+  type AnyLayerSpec = LayerSpecByType<LayerType>;
+
+  type LayerOverride<TSpec> = {
+    spec: DeepPartial<Omit<TSpec, "type">>;
     background?: DeepPartial<Background>;
   };
 
-  export class MapLibreSharedLayer<TType extends LayerType> {
-    spec: LayerSpecByType<TType>;
+  export class MapLibreSharedLayer<TSpec extends AnyLayerSpec> {
+    spec: TSpec;
     background?: Background = $state();
-    overrides: SvelteMap<string, LayerOverride<TType>> = new SvelteMap();
+    overrides: SvelteMap<string, LayerOverride<TSpec>> = new SvelteMap();
 
-    constructor(spec: LayerSpecByType<TType>, background?: Background, overrides: [string, LayerOverride<TType>][] = []) {
+    constructor(spec: TSpec, background?: Background, overrides: [string, LayerOverride<TSpec>][] = []) {
       this.spec = $state(spec);
       this.background = background;
       overrides.forEach(([key, override]) => this.addOverride(key, override))
     }
 
-    addOverride(key: string, override: LayerOverride<TType>) {
+    addOverride(key: string, override: LayerOverride<TSpec>) {
       let spec = $state(override?.spec ?? {});
-      spec.type ??= this.spec.type;
       let background = $state(override?.background);
       this.overrides.set(key, {spec, background});
     }
@@ -89,32 +83,40 @@
 
   // I have some source URLs, I wanna make some shared layers
   // createLayerFromSource() --> given a MapLibreSource, create a layer spec, feed into sharedlayer
-  const mapLibreSharedLayers: SvelteMap<string, MapLibreSharedLayer<LayerType>> = new SvelteMap();
+  const mapLibreSharedLayers: SvelteMap<string, MapLibreSharedLayer<AnyLayerSpec>> = new SvelteMap();
 
+  const spec: LayerSpecByType<"hillshade"> = {
+    source: "0",  // source will be the key to the sources.map
+    type: "hillshade",
+    paint: {
+      "hillshade-exaggeration": 0.3,
+      "hillshade-illumination-direction": 180,
+    }
+  }
   const hillshadeSharedLayer = new MapLibreSharedLayer(
-    {
-      source: "0",  // source will be the key to the sources.map
-      type: "hillshade",
-      paint: {
-        "hillshade-exaggeration": 0.3,
-        "hillshade-illumination-direction": 180,
-      }
-    } satisfies LayerSpecByType<"hillshade">,
+    spec,
     undefined,
     [
-      ["00", {spec: {}}],
+      ["00", {spec: {
+        paint: {
+          "raster-opacity": 0.3
+        }
+      }}],
       ["11", {spec: {}}],
       ["22", {spec: {}}],
     ]
-    // defineLayer({
-    //   source: "0",  // source will be the key to the sources.map
-    //   type: "hillshade",
-    // }),
   )
 
   hillshadeSharedLayer.addOverride(
     "33",
-    {spec: {paint: {"hillshade-exaggeration": 0.8}}},
+    {
+      spec: {
+        paint: {
+          "hillshade-exaggeration": 0.8,
+          "raster-opacity": 0.3,
+        }
+      }
+    },
   )
   mapLibreSharedLayers.set(crypto.randomUUID(), hillshadeSharedLayer)
 
@@ -126,7 +128,7 @@
 {#each sharedLayer.overrides as [overrideKey, override] (overrideKey)}
   <div style:user-select=none>Layer: {overrideKey}</div>
   {#if sharedLayer.spec.type === "hillshade"}
-  {#each Object.keys(sharedLayer.spec?.paint) as property (property)}
+  {#each sharedLayer.spec?.paint ? Object.keys(sharedLayer.spec?.paint) : [] as property (property)}
   <div style:display=flex>
     <p>{property}</p>
     {#if typeof override.spec?.paint?.[property] !== "undefined"}
