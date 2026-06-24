d317cc20537d0fd927a3d6c7c02732daf006f647
Lars Maxfield <maxfield.lars@gmail.com>
2026-06-24 12:05:25 +0200

    Add layer group entries by source proof
    We're almost there

--- src/routes/test/source-manager-svelte-maplibre-gl/+page.svelte
+++ src/routes/test/source-manager-svelte-maplibre-gl/+page.svelte
@@ -13,6 +13,7 @@
 
   const localUrl = new URL('/local/bagunca-2025-10-21T1629/rgb.pmtiles', import.meta.url);
   const localUrlDem = new URL('/local/bagunca-2025-10-21T1629/height.pmtiles', import.meta.url);
+  // const initialUrls = [localUrl, localUrl, localUrlDem]
   const initialUrls = [localUrl, localUrlDem]
 
   function sourceFromUrl(url: URL): PMTilesTileset {
@@ -44,6 +45,7 @@
 
   // Proof:
   import { onMount } from "svelte";
+  import { symbolName } from "typescript";
 
   let nViewers = $state(2);
   // svelte-ignore state_referenced_locally
@@ -59,35 +61,66 @@
       ...await mapLibreSource.source.spec,
       ...mapLibreSource?.override
     }
-    const layerSpecType = sourceSpec.type === "raster" ? "raster" : "hillshade";
-    const initialPaintSpec =
-      layerSpecType === "raster" ?
-      {
+
+    if (sourceSpec.type === "raster") {  // If raster, add a raster layer to each group
+      const layerSpecType = "raster";
+      const initialPaintSpec = {
         "raster-opacity": 0.5,
-      } :
-      {
-        "hillshade-exaggeration": 0.5,
-        "hillshade-illumination-direction": 315,
       }
-    const layerSpec: AnyLayerSpec = {  // This isn't state(); the MapLibreSyncedLayer.spec is.
-      source: mapLibreSourceKey,
-      type: layerSpecType,
-      paint: initialPaintSpec,
+      const layerSpec: AnyLayerSpec = {  // This isn't state(); the MapLibreSyncedLayer.spec is.
+        source: mapLibreSourceKey,
+        type: layerSpecType,
+        paint: initialPaintSpec,
+      }
+      const overrides = [...Array(nOverrides).keys()].map(() => [crypto.randomUUID(), {spec: {}}] satisfies [string, LayerOverride<AnyLayerSpec>])
+      const syncedLayer = new MapLibreSyncedLayer(
+        layerSpec,
+        undefined,
+        overrides,
+      )
+      const syncedLayerKey = crypto.randomUUID();
+      syncedMapLibreLayers.set(syncedLayerKey, syncedLayer);
+      const overrideKeys = [...syncedLayer.overrides.keys()];
+      layerGroups.order.forEach((layerGroupKey, index) => {
+        layerGroups.map.get(layerGroupKey)?.add(syncedLayerKey, {key: overrideKeys.at(index)})
+      })
+    } else if (sourceSpec.type === "raster-dem") {  // If raster-dem, add a hillshade AND color-relief layer to each group
+      const layerSpecTypes = ["hillshade", "color-relief"];
+      layerSpecTypes.forEach((layerSpecType) => {
+        const initialPaintSpec =
+          layerSpecType === "hillshade" ?
+          {
+            "hillshade-exaggeration": 0.5,
+            "hillshade-illumination-direction": 315,
+          } :
+          {
+            'color-relief-color': [
+              'interpolate',
+              ['linear'],
+              ['elevation'],
+              0, 'rgb(4, 0, 108)',
+              5000, 'rgb(215, 5, 13)'
+            ]
+          }
+        const layerSpec: AnyLayerSpec = {  // This isn't state(); the MapLibreSyncedLayer.spec is.
+          source: mapLibreSourceKey,
+          type: layerSpecType,
+          paint: initialPaintSpec,
+        }
+        const overrides = [...Array(nOverrides).keys()].map(() => [crypto.randomUUID(), {spec: {}}] satisfies [string, LayerOverride<AnyLayerSpec>])
+        const syncedLayer = new MapLibreSyncedLayer(
+          layerSpec,
+          undefined,
+          overrides,
+        )
+        const syncedLayerKey = crypto.randomUUID();
+        syncedMapLibreLayers.set(syncedLayerKey, syncedLayer);
+        const overrideKeys = [...syncedLayer.overrides.keys()];
+        layerGroups.order.forEach((layerGroupKey, index) => {
+          layerGroups.map.get(layerGroupKey)?.add(syncedLayerKey, {key: overrideKeys.at(index)})
+        })
+      })
     }
-    const overrides = [...Array(nOverrides).keys()].map(() => [crypto.randomUUID(), {spec: {}}] satisfies [string, LayerOverride<AnyLayerSpec>])
-    const syncedLayer = new MapLibreSyncedLayer(
-      layerSpec,
-      undefined,
-      overrides,
-    )
-    const syncedLayerKey = crypto.randomUUID();
-    syncedMapLibreLayers.set(syncedLayerKey, syncedLayer);
-    const overrideKeys = [...syncedLayer.overrides.keys()];
-    layerGroups.order.forEach((layerGroupKey, index) => {
-      layerGroups.map.get(layerGroupKey)?.add(syncedLayerKey, {key: overrideKeys.at(index)})
-    })
-    // layerGroups.map.get(layerGroups.order.at(0))?.add(syncedLayerKey, {key: overrideKeys.at(0)})
-    // layerGroups.map.get(layerGroups.order.at(1))?.add(syncedLayerKey, {key: overrideKeys.at(1)})
   }
 
   onMount(() => {
@@ -97,10 +130,6 @@
     // For each source, create a synced layer derived from its spec with as many
     // overrides as nViewers, to simulate side by side with the same source
     initialSourceKeys.forEach((sourceKey) => {
-      // const mapLibreSource = sourceManager.mapLibreSources.get(sourceKey);
-      // console.log(
-      //   mapLibreSource?.source
-      // )
       deriveSyncedLayerFromMapLibreSource(sourceKey, nViewers)
     })
     // addLayer()
@@ -108,17 +137,103 @@
 
 </script>
 
-<h2>List of layer groups and their respective (override) layers</h2>
-{#each layerGroups.map as [layerGroupKey, layerGroup], iGroup (layerGroupKey)}
-  <h3>Viewer (layer group): {iGroup}</h3>
-  {#each layerGroup.map as [overrideKey, syncedLayerKey] (overrideKey)}
-    <div>
-      {syncedMapLibreLayers.get(syncedLayerKey)?.spec.type}
+<div style:display=grid style:grid-template-columns="1fr auto 0fr">
+  <div style:grid-column="-1 / 1">
+    Add layer(s) from
+    <button onclick={() => deriveSyncedLayerFromMapLibreSource([...sourceManager.mapLibreSources.keys()][0], nViewers)}>raster source</button>
+    <button onclick={() => deriveSyncedLayerFromMapLibreSource([...sourceManager.mapLibreSources.keys()][1], nViewers)}>raster-dem source</button>
+  </div>
+  <div style:grid-column="-1 / 1">
+    <h2>List of layer group overrides sorted by source</h2>
+  </div>
+  <div style:display=grid style:grid-template-columns=subgrid style:grid-column="-1 / 1">
+    <div>Property</div>
+    <div>Value</div>
+    <div>Sync?</div>
+  </div>
+  {#each layerGroups.map as [layerGroupKey, layerGroup], iGroup (layerGroupKey)}
+    <div style:grid-column="-1 / 1" style:border-top="1px solid white">
+      <h3>Viewer (layer group): {iGroup}</h3>
     </div>
+    <!-- Reduce across the overrideKeys, the source keys -->
+    {@const layerGroupEntriesBySource = layerGroup.map.entries().reduce(
+      (uniqueSources, [overrideKey, syncedLayerKey]) => {
+        const syncedLayer = syncedMapLibreLayers.get(syncedLayerKey);
+        const sourceKey = syncedLayer?.overrides.get(overrideKey)?.spec?.source ?? syncedLayer?.spec.source;
+        if (uniqueSources.get(sourceKey) === undefined) {
+          const sourceLayerGroup = new Map([[overrideKey, syncedLayerKey]]);
+          uniqueSources.set(sourceKey, sourceLayerGroup);
+        } else {
+          uniqueSources.get(sourceKey).set(overrideKey, syncedLayerKey)
+        }
+        return uniqueSources
+      },
+      new Map()
+      )
+    }
+    {#each layerGroupEntriesBySource as [sourceKey, layerGroupEntries] (sourceKey)}
+      <div style:grid-column="-1 / 1" style:border-top="1px solid gray">
+        <h4>Source: {sourceKey}</h4>
+      </div>
+      {#each layerGroupEntries.entries() as [overrideKey, syncedLayerKey]}
+        <div>{syncedMapLibreLayers.get(syncedLayerKey)?.spec.type}</div>
+        <div>{syncedLayerKey}</div>
+        <div></div>
+      {/each}
+    {/each}
   {/each}
-{/each}
+</div>
 
-<h2>List of synced layers and their respective overrides</h2>
+
+<!-- Prove sidebar Layer Manager -->
+<div style:display=grid style:grid-template-columns="1fr auto 0fr">
+  <div style:grid-column="-1 / 1">
+    <h2>List of layer groups and their respective (override) layers</h2>
+  </div>
+  <div style:display=grid style:grid-template-columns=subgrid style:grid-column="-1 / 1">
+    <div>Property</div>
+    <div>Value</div>
+    <div>Sync?</div>
+  </div>
+  {#each layerGroups.map as [layerGroupKey, layerGroup], iGroup (layerGroupKey)}
+    <div style:grid-column="-1 / 1" style:border-top="1px solid white">
+      <h3>Viewer (layer group): {iGroup}</h3>
+    </div>
+    {#each [...layerGroup.map.entries()].reverse() as [overrideKey, syncedLayerKey] (overrideKey)}
+      {@const syncedLayer = syncedMapLibreLayers.get(syncedLayerKey)}
+      {@const override = syncedLayer?.overrides.get(overrideKey)}
+      <div style:grid-column="-1 / 1">
+        <div>
+          {syncedMapLibreLayers.get(syncedLayerKey)?.spec.type}
+        </div>
+      </div>
+      {#each Object.keys(syncedLayer.spec?.paint ?? {}) as property (property)}
+        <div style:display=grid style:grid-template-columns=subgrid style:grid-column="-1 / 1">
+          <div>{property}</div>
+          <div>
+            {#if typeof override.spec?.paint?.[property] !== "undefined"}
+              <input type=range bind:value={override.spec.paint[property]} style:user-select=none/>
+            {:else if typeof syncedLayer.spec.paint?.[property] !== "undefined"}
+              <input type=range bind:value={syncedLayer.spec.paint[property]} style:user-select=none/>
+            {/if}
+          </div>
+          <div style:align-self=center style:justify-self=center>
+            <input type=checkbox checked={override.spec?.paint?.[property] === undefined} onchange={(e) => {
+              if (e.target.checked) {
+                delete override.spec.paint[property];
+              } else {
+                override.spec.paint = {...override.spec?.paint, [property]: syncedLayer.spec.paint[property]}
+              }
+            }}>
+          </div>
+        </div>
+
+      {/each}
+    {/each}
+  {/each}
+</div>
+
+<!-- <h2>List of synced layers and their respective overrides</h2>
 {#each syncedMapLibreLayers as [syncedLayerKey, syncedLayer]}
   <h3>{syncedLayer.spec.type} {syncedLayerKey}</h3>
   {#each syncedLayer.overrides as [overrideKey, override]}
@@ -142,7 +257,7 @@
 
     {/each}
   {/each}
-{/each}
+{/each} -->
 
 
 <!-- MapLibre-knowing?
@@ -178,3 +293,53 @@ Class LayerManager
     <source>
       <terrain>
 -->
+
+<style>
+  /*
+    Josh's Custom CSS Reset
+    https://www.joshwcomeau.com/css/custom-css-reset/
+  */
+
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  *:not(dialog) {
+    margin: 0;
+  }
+
+  @media (prefers-reduced-motion: no-preference) {
+    html {
+      interpolate-size: allow-keywords;
+    }
+  }
+
+  body {
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  img, picture, video, canvas, svg {
+    display: block;
+    max-width: 100%;
+  }
+
+  input, button, textarea, select {
+    font: inherit;
+  }
+
+  p, h1, h2, h3, h4, h5, h6 {
+    overflow-wrap: break-word;
+  }
+
+  p {
+    text-wrap: pretty;
+  }
+  h1, h2, h3, h4, h5, h6 {
+    text-wrap: balance;
+  }
+
+  #root, #__next {
+    isolation: isolate;
+  }
+</style>
