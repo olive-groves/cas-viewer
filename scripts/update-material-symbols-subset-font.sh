1b09c44d86b738256b3e5dc424c160cb9d12082d
Lars Maxfield <maxfield.lars@gmail.com>
2026-07-29 16:23:57 +0200

    Fix material symbols script
    Spoof user-agent that supports font variations FILL, GRAD, etc.

--- scripts/update-material-symbols-subset-font.sh
+++ scripts/update-material-symbols-subset-font.sh
@@ -13,13 +13,15 @@ icons+=("settings")
 
 icons_string=$(IFS=,; echo "${icons[*]}")
 
-css_url="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:FILL@0..1&icon_names=${icons_string}&display=block"
+css_url="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:FILL,GRAD,opsz@0..1,-25..200,20..48&icon_names=${icons_string}&display=block"
+# Spoof a FILL- and GRAD-supporting user agent
+user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv125.0) Gecko/20100101 Firefox/125.0"
 
 font_url=$(
-  curl -s "$css_url" |
+  curl -A "$user_agent" -s "$css_url" |
   sed -nE 's/.*url\(([^)]*)\).*/\1/p' |
   head -n1 |
   tr -d '"'\'' '
 )
 
-curl -L "$font_url" > ./static/fonts/material-symbols-sharp.subset.woff2
+curl -L "$font_url" -o ./static/fonts/material-symbols-sharp.subset.woff2
