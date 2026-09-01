07481010ccadd11648b7e8c4e86e0b5749ef316f
Lars Maxfield <83759569+larsmaxfield@users.noreply.github.com>
2026-09-01 11:01:43 +0200

    Add MiniMapViewer notes

---
+++ src/lib/v0.8/MiniMapViewer.svelte
@@ -0,0 +1,16 @@
+<!-- MiniMapViewer
+
+It displays where all views are with rectangles on the zoomed-out layer(s).
+- It therefore knows all views and their cameras.
+
+If all the views are synced, it'll just show a single rectangle.
+If a view is not synced, it will show that rectangle separately.
+
+The zoomed-out layer(s) are selected by heuristics:
+- The first raster, hillshade, or color-relief layer of the views.
+- The first layer is found by traversing the order of layers in the order of views, including nested views (multiviews). If the first view is a multiview, it will traverse that multiview before proceeding to the second view.
+
+
+The zoom is not set. Rather, a transformConstrain that keeps the map filled in view.
+Zoom in/out if resize; keep the bounds within view.
+ -->
