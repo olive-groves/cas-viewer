118a81ebefd99d57ccbd95fde7a98a8e1b0d6eab
Lars Maxfield <maxfield.lars@gmail.com>
2026-09-02 11:43:46 +0200

    Remove reset button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,6 @@
 <nav style:margin="0" style:padding="0.3em">
   <button onclick={() => {window.location.reload(); window.location.href = '/';}}><h1>PMTiles Multi Viewer</h1></button>
   <a class=home href="/almond-blossom">Harm Belt's <em>Almond Blossom</em></a>
-  <button onclick={() => {window.location.reload();}}><p>reset</p></button>
   <!-- <a href="{base}/test-openstreetmap">test</a> -->
   <!-- <button class="restart" onclick={() => {window.location.reload(); window.location.href = `${base}/`;}}>reset</button> -->
 </nav>
