94c32d773aa8b49f58520e5de119f3e46668b4f7
Lars Maxfield <maxfield.lars@gmail.com>
2026-09-07 11:37:41 +0200

    SyncedTerraDraw: Improve classes; ready for lib

--- src/routes/test/terra-draw/sync/+page.svelte
+++ src/routes/test/terra-draw/sync/+page.svelte
@@ -8,6 +8,7 @@
     TerraDrawSelectMode,
     TerraDrawPolygonMode,
     TerraDrawPointMode,
+    TerraDrawPolyLineMode,
     type TerraDrawEventListeners,
   } from 'terra-draw';
   import { TerraDrawMapLibreGLAdapter } from "terra-draw-maplibre-gl-adapter";
@@ -16,8 +17,6 @@
   import { isGeometryOutOfBounds, terraDrawMaxBounds, wrapGeometryCoordinatesToBounds } from '$lib/v0.8/geojson-utils';
   import { onMount } from 'svelte';
   import { SvelteMap } from 'svelte/reactivity';
-
-  type FeatureId = string | number;
 
   // Shared state, options
   const defaultSelectFlags = {
@@ -33,12 +32,25 @@
   const selectFlags = {
     point: defaultSelectFlags,
     polygon: defaultSelectFlags,
+    polyline: defaultSelectFlags,
   }
+
+  type Mode = TerraDrawSelectMode | TerraDrawPointMode | TerraDrawPolygonMode | TerraDrawPolyLineMode;
+
+  type ModeFactory = () => Mode[];
 
   class SyncedTerraDraw {
     mode = $state('point');
-    modes: (TerraDrawSelectMode | TerraDrawPointMode | TerraDrawPolygonMode)[];
+    modes: Mode[];
+    private modeFactory: ModeFactory;
     selected: string | number | null = $state(null);
+
+    onreadyListeners: TerraDrawEventListeners["ready"][] = [];
+    onfinishListeners: TerraDrawEventListeners["finish"][] = [];
+    onchangeListeners: TerraDrawEventListeners["change"][] = [];
+    onselectListeners: TerraDrawEventListeners["select"][] = [];
+    ondeselectListeners: TerraDrawEventListeners["deselect"][] = [];
+    onhistoryListeners: TerraDrawEventListeners["history"][] = [];
 
     // For retaining a DOM element
     // TODO: Make private?
@@ -48,11 +60,12 @@
 
     instances = new SvelteMap<string, TerraDrawInstance>();
 
-    constructor() {
+    constructor(modeFactory: ModeFactory) {
       $effect(() => {
-        this.draw?.setMode(syncedTerraDraw.mode);
+        this.draw?.setMode(this.mode);
       });
-      this.modes = SyncedTerraDraw.modesFactory();
+      this.modeFactory = modeFactory;
+      this.modes = modeFactory();
     }
 
     static outOfBoundsValidator = (feature, { updateType }) => {
@@ -64,64 +77,51 @@
       return { valid: true }
     }
 
-    static modesFactory() {
-      return [
-        new TerraDrawSelectMode({
-          flags: selectFlags,
-        }),
-        new TerraDrawPointMode({
-          validation: this.outOfBoundsValidator,
-        }),
-        new TerraDrawPolygonMode({
-          validation: this.outOfBoundsValidator,
-        }),
-      ];
-    }
-
-    addInstance(givenId?: string): TerraDrawInstance {
-      const id = givenId ?? crypto.randomUUID();
-      let instance = new TerraDrawInstance(id);
+    addInstance(customId?: string): TerraDrawInstance {
+      const id = customId ?? crypto.randomUUID();
+      let instance = new TerraDrawInstance(id, this.modeFactory());
+      instance.onreadyListeners.push((...args) => this.synconready(instance.id, args));
+      instance.onselectListeners.push((...args) => this.synconselect(instance.id, args));
+      instance.ondeselectListeners.push((...args) => this.syncondeselect(instance.id, args));
+      instance.onfinishListeners.push((...args) => this.synconfinish(instance.id, args));
+      instance.onchangeListeners.push((...args) => this.synconchange(instance.id, args));
+      // FIXME: Set timeout, await, or some other trigger for setting snapshot?
+      // 'ready' doesn't fire on first component instantiation; only after explicit stop-start (hide-show)
+      // instance.onreadyListeners.push(() => instance.draw?.addFeatures(this.draw?.getSnapshot() ?? []));
+      setTimeout(() => {
+        instance.draw?.addFeatures(this.draw?.getSnapshot() ?? []);
+      }, 500)
       this.instances.set(id, instance);
-      instance.onselect = (featureId) => this.propagateonselect([featureId], instance.id);
-      instance.ondeselect = (featureId) => this.propagateondeselect([featureId], instance.id);
-      instance.onfinish = (featureId, context) => this.propagateonfinish([featureId, context], instance.id);
-      instance.onchange = (ids: FeatureId[], type: string, context?) => {console.log("onchange", ids, type, context)}
-      // Set timeout, await, or some other trigger for setting snapshot?
-      const snapshot = this.draw?.getSnapshot();
-      if (snapshot) {
-        // Await on draw instanced?
-        setTimeout(() => {
-          instance.draw?.addFeatures(snapshot);
-        }, 500)
-      }
       return instance
     }
 
-    propagateonselect = (args: Parameters<TerraDrawEventListeners["select"]>, instanceId: string) => {
-      console.log("onselect", ...args);
-      this.onselect(...args);
-      // this.instances.forEach((instance, id) => {})
+    synconready = (id: string, args: Parameters<TerraDrawEventListeners["ready"]>) => {
+      console.log("synconready", ...args);
     }
-    propagateondeselect = (args: Parameters<TerraDrawEventListeners["deselect"]>, instanceId: string) => {
-      console.log("ondeselect", ...args);
-      this.ondeselect(...args);
-      // Bug: Press Escape when dragging a selected feature; onfinish returns undefined id; catch on deselect
-      const instance = this.instances.get(instanceId);
+
+    synconselect = (id: string, args: Parameters<TerraDrawEventListeners["select"]>) => {
+      console.log("synconselect", ...args);
+      this.selected = id;
+    }
+    syncondeselect = (id: string, args: Parameters<TerraDrawEventListeners["deselect"]>) => {
+      console.log("syncondeselect", ...args);
+      this.selected = null;
+      // This fixes a bug when pressing Escape (cancelling) when dragging a selected feature; onfinish returns undefined id; catch on deselect
+      const instance = this.instances.get(id);
       const featureId = args[0];
       let feature = instance?.draw?.getSnapshotFeature(featureId);
       if (feature) {
         this.draw?.updateFeatureGeometry(featureId, feature.geometry);
         this.instances.forEach((_instance, _id) => {
-          if (_id === instanceId)
+          if (_id === id)
             return;
           _instance.draw?.updateFeatureGeometry(featureId, feature.geometry);
         })
       }
     }
-    propagateonfinish = (args: Parameters<TerraDrawEventListeners["finish"]>, instanceId: string) => {
-      console.log("onfinish", ...args);
-      this.onfinish(...args);
-      const instance = this.instances.get(instanceId);
+    synconfinish = (id: string, args: Parameters<TerraDrawEventListeners["finish"]>) => {
+      console.log("synconfinish", ...args);
+      const instance = this.instances.get(id);
       const featureId = args[0];
       const context = args[1];
       if (!instance || typeof featureId === "undefined")
@@ -131,7 +131,7 @@
         if (feature) {
           this.draw?.addFeatures([feature]);
           this.instances.forEach((_instance, _id) => {
-            if (_id === instanceId)
+            if (_id === id)
               return;
             _instance.draw?.addFeatures([feature]);
           })
@@ -145,62 +145,51 @@
           instance.draw?.updateFeatureGeometry(featureId, feature.geometry);  // Ensure drawn is wrapped
           this.draw?.updateFeatureGeometry(featureId, feature.geometry);  // Then propogate
           this.instances.forEach((_instance, _id) => {
-            if (_id === instanceId)
+            if (_id === id)
               return;
             _instance.draw?.updateFeatureGeometry(featureId, feature.geometry);
           })
         }
       }
     }
-
-    onselect: TerraDrawEventListeners["select"] = (id: FeatureId) => {
-      this.selected = id;
-    };
-    ondeselect: TerraDrawEventListeners["deselect"] = (id: FeatureId) => {
-      this.selected = null;
-    };
-    onfinish: TerraDrawEventListeners["finish"] = () => {};
-    // onchange: TerraDrawEventListeners["change"] = () => {};
-    // onhistory: TerraDrawEventListeners["history"] = () => {};
-
-  }
-
-  let syncedTerraDraw = new SyncedTerraDraw();
-
-  onMount(() => {
-    if (document.getElementById(syncedTerraDraw.id) === null) {
-      const container = document.createElement("div");
-      container.id = syncedTerraDraw.id;
-      document.body.appendChild(container);
-      syncedTerraDraw.map = new MapLibreMap({
-        container: container,
-      });
-      syncedTerraDraw.draw = new TerraDraw({
-        adapter: new TerraDrawMapLibreGLAdapter({map: syncedTerraDraw.map}),
-        modes: syncedTerraDraw.modes,
-      });
-      syncedTerraDraw.draw.start();
+    synconchange = (id: string, args: Parameters<TerraDrawEventListeners["change"]>) => {
+      console.log("synconchange", ...args);
     }
-  })
+
+    readonly onready: TerraDrawEventListeners["ready"] = (...args) => this.onreadyListeners.forEach((listener) => listener(...args));
+    readonly onfinish: TerraDrawEventListeners["finish"] = (...args) => this.onfinishListeners.forEach((listener) => listener(...args));
+    readonly onchange: TerraDrawEventListeners["change"] = (...args) => this.onchangeListeners.forEach((listener) => listener(...args));
+    readonly onselect: TerraDrawEventListeners["select"] = (...args) => this.onselectListeners.forEach((listener) => listener(...args));
+    readonly ondeselect: TerraDrawEventListeners["deselect"] = (...args) => this.ondeselectListeners.forEach((listener) => listener(...args));
+    readonly onhistory: TerraDrawEventListeners["history"] = (...args) => this.onhistoryListeners.forEach((listener) => listener(...args));
+  }
 
   class TerraDrawInstance {
     id: string;
     draw: TerraDraw | undefined = $state.raw();
-    modes: (TerraDrawSelectMode | TerraDrawPointMode | TerraDrawPolygonMode)[];
+    modes: Mode[];
     private snapshot: ReturnType<TerraDraw["getSnapshot"]> = [];
     private _visible: boolean = $state(true);
     readonly visible = $derived(this._visible);
 
-    constructor(id: string) {
+    onreadyListeners: TerraDrawEventListeners["ready"][] = [];
+    onfinishListeners: TerraDrawEventListeners["finish"][] = [];
+    onchangeListeners: TerraDrawEventListeners["change"][] = [];
+    onselectListeners: TerraDrawEventListeners["select"][] = [];
+    ondeselectListeners: TerraDrawEventListeners["deselect"][] = [];
+    onhistoryListeners: TerraDrawEventListeners["history"][] = [];
+
+    constructor(id: string, modes: Mode[]) {
       this.id = id;
-      this.modes = SyncedTerraDraw.modesFactory();
+      this.modes = modes;
     }
 
-    onselect: TerraDrawEventListeners["select"] = () => {};
-    ondeselect: TerraDrawEventListeners["deselect"] = () => {};
-    onfinish: TerraDrawEventListeners["finish"] = () => {};
-    onchange: TerraDrawEventListeners["change"] = () => {};
-    onhistory: TerraDrawEventListeners["history"] = () => {};
+    readonly onready: TerraDrawEventListeners["ready"] = (...args) => this.onreadyListeners.forEach((listener) => listener(...args));
+    readonly onfinish: TerraDrawEventListeners["finish"] = (...args) => this.onfinishListeners.forEach((listener) => listener(...args));
+    readonly onchange: TerraDrawEventListeners["change"] = (...args) => this.onchangeListeners.forEach((listener) => listener(...args));
+    readonly onselect: TerraDrawEventListeners["select"] = (...args) => this.onselectListeners.forEach((listener) => listener(...args));
+    readonly ondeselect: TerraDrawEventListeners["deselect"] = (...args) => this.ondeselectListeners.forEach((listener) => listener(...args));
+    readonly onhistory: TerraDrawEventListeners["history"] = (...args) => this.onhistoryListeners.forEach((listener) => listener(...args));
 
     hide() {
       if (!this.draw) {
@@ -224,6 +213,53 @@
   }
 
   // Proof
+
+  // TODO: Svelte component on the SingleViewer side? Or MultiViewer side?
+  // We just need a place to call this onMount. So if we pack it in a MultiViewer...?
+  // ONE syncedTerraDraw instance could be spread across nested multi-viewers. We
+  // don't necessarily want to spam that component. What if we made it a shared instance?
+  // Then, whenever we use the shared instance (at the root level), we have to declare
+  // that component:
+  //   <SyncedTerraDraw {...syncedTerraDraw} />
+  // If there are MULTIPLE synced draws?
+  //  #each syncedTerraDraws (SvelteMap?)
+  //    <SyncedTerraDraw {...draw} /> (we just need onMount to be called for each)
+  // For now just one :|
+  onMount(() => {
+    if (document.getElementById(syncedTerraDraw.id) === null) {
+      const container = document.createElement("div");
+      container.id = syncedTerraDraw.id;
+      document.body.appendChild(container);
+      syncedTerraDraw.map = new MapLibreMap({  // bind:map={syncedDraw.map}
+        container: container,
+      });
+      syncedTerraDraw.draw = new TerraDraw({  // bind:draw={syncedDraw.draw}
+        adapter: new TerraDrawMapLibreGLAdapter({map: syncedTerraDraw.map}),
+        modes: syncedTerraDraw.modes,
+      });
+      syncedTerraDraw.draw.start();
+    }
+  })
+
+  const modeFactory = () => {
+    {
+      return [
+        new TerraDrawSelectMode({
+          flags: selectFlags,
+        }),
+        new TerraDrawPointMode({
+          validation: SyncedTerraDraw.outOfBoundsValidator,
+        }),
+        new TerraDrawPolygonMode({
+          validation: SyncedTerraDraw.outOfBoundsValidator,
+        }),
+        new TerraDrawPolyLineMode({
+          validation: SyncedTerraDraw.outOfBoundsValidator,
+        }),
+      ];
+    }
+  }
+  let syncedTerraDraw = new SyncedTerraDraw(modeFactory);
   const modeNames = syncedTerraDraw.modes.map((mode) => mode.mode);
   let zoom = $state(0)
   let center = $state([0, 0])
@@ -254,17 +290,17 @@
         layout={{visibility: "visible"}}
         paint={{"background-color": `rgb(${(Math.random()*255).toFixed(0)}, ${(Math.random()*255).toFixed(0)}, ${(Math.random()*255).toFixed(0)})`}}
       />
-      {#if instance.visible}
-        <TerraDrawSvelte
-          // FIXME: hide/show throws error with already registered
-          mode={syncedTerraDraw.mode}
-          modes={instance.modes}
-          bind:draw={instance.draw}
-          onselect={instance.onselect}
-          ondeselect={instance.ondeselect}
-          onfinish={instance.onfinish}
-        />
-      {/if}
+      <!-- FIXME: show, hide, show throws error with already registered -->
+      <TerraDrawSvelte
+        mode={syncedTerraDraw.mode}
+        modes={instance.modes}
+        bind:draw={instance.draw}
+        onready={(instance.onready)}
+        onfinish={instance.onfinish}
+        onselect={instance.onselect}
+        ondeselect={instance.ondeselect}
+        onchange={instance.onchange}
+      />
     </MapLibre>
     {/each}
   </div>
